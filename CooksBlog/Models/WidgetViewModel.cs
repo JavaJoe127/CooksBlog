c4690be73a8fa67d66fd0f3de4a1cd52c5d029bf
L. Joseph Poyner <javajoe127@gmail.com>
2015-11-05 08:54:40 -0700

    Add tags
    List out all tags in sidebar

--- CooksBlog/Models/WidgetViewModel.cs
+++ CooksBlog/Models/WidgetViewModel.cs
@@ -25,12 +25,18 @@ namespace CooksBlog.Models
         public IList<Category> Categories { get; private set; }
 
         /// <summary>
+        /// The Get or Set Tags
+        /// </summary>
+        public IList<Tag> Tags { get; private set; }
+
+        /// <summary>
         /// The Widget View Model class constructor
         /// </summary>
         /// <param name="blogRepository"></param>
         public WidgetViewModel(IBlogRepository blogRepository)
         {
             Categories = blogRepository.Categories();
+            Tags = blogRepository.Tags();
         }
     }
 }
